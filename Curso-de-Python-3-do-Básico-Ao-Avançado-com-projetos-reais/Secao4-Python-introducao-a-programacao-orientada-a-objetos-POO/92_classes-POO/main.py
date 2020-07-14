0a2572de5a36b12163fbd9ab32f13736a6e1325e
Pedro Henrique <pedrohdpf21@gmail.com>
2020-07-14 15:43:59 -0300

    Subindo aulas

---
+++ Curso-de-Python-3-do-Básico-Ao-Avançado-com-projetos-reais/Secao4-Python-introducao-a-programacao-orientada-a-objetos-POO/92_classes-POO/main.py
@@ -0,0 +1,40 @@
+from pessoas import Pessoa
+# p1 = Pessoa()
+# p2 = Pessoa()
+
+# print(p1)
+# print(p2)
+
+# p1.nome = 'Pedro'
+# p2.nome = 'Luiz'
+# print(p1.nome)
+# print(p2.nome)
+# p1.falar()
+
+# inicio codigo
+
+# p1 = Pessoa('Pedro', 23)
+# p1.comer('Maçã')
+# # p1.para_comer()
+# # p1.para_comer()
+# # p1.comer('Maçã')
+#
+# p1.falar('POO')
+# p1.para_comer()
+# p1.falar('POO')
+# p1.comer('Alimento')
+# p1.parar_falar()
+# p1.falar('Assunto')
+
+
+# inicio
+
+p1 = Pessoa('Pedro', 23)
+p2 = Pessoa('Luiz', 40)
+
+p1.falar('Poo')
+p2.comer('sorvete')
+p1.comer('Churrasco')
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
