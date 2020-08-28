af907a22deea910a5bb50f9bd1d7e36d64589a49
Pedro Henrique <pedrohdpf21@gmail.com>
2020-08-27 22:19:25 -0300

    SUB

---
+++ Curso-de-Python-3-do-Básico-Ao-Avançado-com-projetos-reais/Secao4-Python-introducao-a-programacao-orientada-a-objetos-POO/112_docStrings-documentacao/main.py
@@ -0,0 +1,11 @@
+import uma_linha
+import varias_linhas
+import funcaos
+import classes
+import typehits
+import doc_strings
+
+
+help(uma_linha)
+help(varias_linhas)
+help(funcaos)
---
+++ Curso-de-Python-3-do-Básico-Ao-Avançado-com-projetos-reais/Secao4-Python-introducao-a-programacao-orientada-a-objetos-POO/112_docStrings-documentacao/varias_linhas.py
@@ -0,0 +1,14 @@
+"""Descrição rapida e simples
+
+Lorem Ipsum é simplesmente texto fictício da indústria de impressão e
+digitação. Lorem Ipsum tem sido o texto falso padrão da indústria desde
+1500, quando uma impressora desconhecida pegou uma galera do tipo e a
+embaralhou para fazer um livro de espécimes. Sobreviveu não só a cinco
+séculos, mas também o salto para a digitação eletrônica, permanecendo
+essencialmente inalterado. Foi popularizado na década de 1960 com o
+lançamento de folhas de Letraset contendo passagens de Lorem Ipsum, e
+mais recentemente com softwares de publicação de desktop como Aldus
+PageMaker incluindo versões de Lorem Ipsum.
+"""
+
+variavel = 'valor'
