cb6220571d91587d373f24ed28cec16075a26e4d
Pedro Henrique <67549131+pedrohd21@users.noreply.github.com>
2020-09-15 17:16:10 -0300

    Subindo aulas js

---
+++ JavaScript/01basico/27funcoes/script.js
@@ -0,0 +1,24 @@
+// function saudacao(nome, idade=0) {
+//    return `Bom dia ${nome} ${idade} anos`;
+// }
+// const variavel = saudacao('Pedro', 23);
+// console.log(variavel);
+
+
+// function soma(x = 1, y = 1) {
+//    const resultado = x + y
+//    return resultado
+// }
+// console.log(soma(1, 9))
+
+
+//const raiz = function (n) {
+//    return n ** 0.5
+//};
+//console.log(raiz(9));
+//console.log(raiz(3));
+//console.log(raiz(81));
+
+
+// const raiz = n => n ** 0.5; // isso é uma raiz ultra mega simplificada
+// console.log(raiz(81));
