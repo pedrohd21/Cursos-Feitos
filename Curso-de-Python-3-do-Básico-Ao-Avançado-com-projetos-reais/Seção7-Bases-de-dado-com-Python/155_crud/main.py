2e0450bef58e81bf79329c2d883859f3918a13dd
Pedro Henrique <67549131+pedrohd21@users.noreply.github.com>
2020-10-09 14:55:35 -0300

    Subindo aulas

--- Curso-de-Python-3-do-Básico-Ao-Avançado-com-projetos-reais/Seção7-Bases-de-dado-com-Python/155_crud/main.py
+++ Curso-de-Python-3-do-Básico-Ao-Avançado-com-projetos-reais/Seção7-Bases-de-dado-com-Python/155_crud/main.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pymysql.cursors
+from contextlib import contextmanager
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@contextmanager
+def conectar():
+    conexao = pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        # password='cursopython@123',
+        db='clientes',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    try:
+        yield conexao
+    finally:
+        conexao.close()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Add um registro na base de dados
+# with conectar() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'INSERT INTO clientes (nome, sobrenome, idade, peso) VALUES (%s, %s, %s, %s)'
+#         cursor.execute(sql, ('Jack', 'Monroe', 112, 220))
+#         conexao.commit()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Add varios registro na base de dados
+# with conectar() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'INSERT INTO clientes (nome, sobrenome, idade, peso) VALUES (%s, %s, %s, %s)'
+#
+#         dados = [
+#             ('MURIEL', 'FIGUEIREDO', 19, 55),
+#             ('ROSE', 'FIGUEIREDO', 19, 55),
+#             ('JOSÉ', 'FIGUEIREDO', 19, 55),
+#         ]
+#         cursor.executemany(sql, dados)
+#         conexao.commit()
+
+
+# Apaga um registro na base de dados
+# with conectar() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'DELETE FROM clientes WHERE id = %s'
+#         cursor.execute(sql, (6, ))
+#         conexao.commit()
+
+
+# Apaga quantidade determinada de registros
+# with conectar() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'DELETE FROM clientes WHERE id IN (%s, %s, %s)'
+#         cursor.execute(sql, (7, 8, 9))
+#         conexao.commit()
+
+
+# Apaga Registra entre um range
+# with conectar() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'DELETE FROM clientes WHERE id BETWEEN %s AND %s'
+#         cursor.execute(sql, (10, 12))
+#         conexao.commit()
+
+# Atualiza um dados no caso aq, nome
+# with conectar() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'UPDATE clientes SET nome=%s WHERE id=%s'
+#         cursor.execute(sql, ('JOANA', 5))
+#         conexao.commit()
+
+
+# Mostra a lista de clientes
+with conectar() as conexao:
+    with conexao.cursor() as cursor:
+        cursor.execute('SELECT * FROM clientes LIMIT 100')
+        resultado = cursor.fetchall()
+
+        for linha in resultado:
+            print(linha)
