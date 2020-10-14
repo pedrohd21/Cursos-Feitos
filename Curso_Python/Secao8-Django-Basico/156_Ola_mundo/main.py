34f02226c47b9b8982304f362747cfbf614bbccd
Pedro Henrique <67549131+pedrohd21@users.noreply.github.com>
2020-10-14 08:16:05 -0300

    Subindo django

---
+++ Curso_Python/Secao8-Django-Basico/156_Ola_mundo/main.py
@@ -0,0 +1,3 @@
+# django-admin startproject projeto .   # Cria a pasta principal do django
+# python manage.py startapp blog        # Cria o blog
+# python manage.py runserver            # Mostra o django no navegador
