0bfa2b6a17692a4a39278c24382efb848d7de73b
Pedro Henrique <67549131+pedrohd21@users.noreply.github.com>
2020-10-26 20:41:11 -0300

    Atualizando

--- Curso_Python/Secao9-Django-Projeto-Agenda-163-183/projeto_agenda/accounts/models.py
+++ Curso_Python/Secao9-Django-Projeto-Agenda-163-183/projeto_agenda/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from contatos.models import Contato  # importar Contato de contato/views
+from django import forms    #importa os formularios que vai ser utilizado no dashboard
 
-# Create your models here.
+
+class FormContato(forms.ModelForm):
+    class Meta:
+        model = Contato         # representar os campos contato
+        exclude = ('mostrar',)
+
